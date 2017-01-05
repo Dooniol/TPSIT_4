901a1cd6e518315b9b573a9a54bc7ec151de0fc1
Daniel <daniel.marini@outlook.it>
2017-01-05 18:08:20 +0100

    Add files via upload

---
+++ Esercizio_1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package esercizio_1;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Esercizio_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int base=5;
+        int altezza=3;
+        int P;
+        float A;
+        P=base+(altezza*2);
+        A=(base*altezza)/2;
+        System.out.println(P);
+        System.out.println(A);
+    }
+    
+}
